ffc924906df5d9e792f8b9ec20746806ae198d25
LDJESD3-18 <leac162007@gmail.com>
2025-12-18 17:33:29 +0000

    definição das variáveis

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,8 +1,14 @@
-#include<stdio.h> // Acessa a Biblioteca,que faz com que haja entrada e saída de dados
+#include<stdio.h>                 // Acessa a Biblioteca,que faz com que haja entrada e saída de dados
 
-  int main(){   // Função para que o programa funcione
-    char letra1;
-    
+  int main(){                       // Função para que o programa funcione
+    char letra1;                   //Declara o formato da variável letra1 como char(caractere)
+    char codigo1[20];             //Declara o formato da variável letra2 como char[](string ou array sequência de caracteres)
+    char cidade1[20];            //Declara o formato da variável cidade1 como  char[](string ou array,sequência de caracteres)
+    int população;              //Declara o formato da variável população como int (número inteiro decimal))
+    float área;                //Declara o formato da variável área como float (número de ponto fluante(número com casas decimais))  
+    float PIB;                //Declara o formato da variável PIB como float (número de ponto fluante(número com casas decimais))  
+    float área;              //Declara o formato da variável área como float (número de ponto fluante(número com casas decimais))  
+    int pontos_turísticos;  //Declara o formato da variável porntos_turísticos(número inteiro decimal)
 
 
 
