07fb60f9235f276fe2289bac0d53249affe106aa
LDJESD3-18 <leac162007@gmail.com>
2025-12-18 17:37:41 +0000

    definição de Estado

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,16 +2,19 @@
 
   int main(){                       // Função para que o programa funcione
     char letra1;                   //Declara o formato da variável letra1 como char(caractere)
-    char codigo1[20];             //Declara o formato da variável letra2 como char[](string ou array sequência de caracteres)
+    char código1[20];             //Declara o formato da variável código1 como char[](string ou array sequência de caracteres)
     char cidade1[20];            //Declara o formato da variável cidade1 como  char[](string ou array,sequência de caracteres)
-    int população;              //Declara o formato da variável população como int (número inteiro decimal))
-    float área;                //Declara o formato da variável área como float (número de ponto fluante(número com casas decimais))  
-    float PIB;                //Declara o formato da variável PIB como float (número de ponto fluante(número com casas decimais))  
-    float área;              //Declara o formato da variável área como float (número de ponto fluante(número com casas decimais))  
-    int pontos_turísticos;  //Declara o formato da variável porntos_turísticos(número inteiro decimal)
-
-
-
+    int população1;              //Declara o formato da variável população1 como int (número inteiro decimal))
+    float área1;                //Declara o formato da variável área1 como float (número de ponto fluante(número com casas decimais))  
+    float PIB1;                //Declara o formato da variável PIB1 como float (número de ponto fluante(número com casas decimais))  
+    float área1;              //Declara o formato da variável área1 como float (número de ponto fluante(número com casas decimais))  
+    int pontos_turísticos1;  //Declara o formato da variável porntos_turísticos1(número inteiro decimal)
+    
+    printf("Super Trunfo em C \n");
+    printf("Carta 1:");
+    printf("Estado:");
+    scanf("%c",letra1)
+    
 
 
 
