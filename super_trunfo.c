3aa58c6cb44f96ecf6bac50c210e909eeb3bc866
LDJESD3-18 <leac162007@gmail.com>
2025-12-18 22:51:11 +0000

    projeto Final

--- super_trunfo.c
+++ super_trunfo.c
@@ -39,7 +39,7 @@
 
 
     printf("Área:");                                                                   //Pede ao usuário o número da área da cidade 
-    scanf(" \n%f",&area1);                                                             // Armazena o valor da área da cidade na variável área1
+    scanf("\n%f",&area1);                                                             // Armazena o valor da área da cidade na variável área1
 
     printf("PIB:");                                                                  //Pede ao usuário o número do Produto Interno Produto(PIB) 
     scanf("\n %f",&PIB1);                                                           //Armazena o valor de PIB  em PIB1
