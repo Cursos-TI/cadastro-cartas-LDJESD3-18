bfd518eae878401c6c9084f34b322ba676d12645
LDJESD3-18 <leac162007@gmail.com>
2025-12-18 17:17:24 +0000

    incio do programa

---
+++ super_trunfo.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // Acessa a Biblioteca,que faz com que haja entrada e saída de dados
+
+  int main(){   // Função para que o programa funcione
+    char letra1;
+    
+
+
+
+
+
+
+  }
