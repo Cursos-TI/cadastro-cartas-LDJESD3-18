a3173b7f603da42ded6cda9f54b74b95067f4d2f
LDJESD3-18 <leac162007@gmail.com>
2026-01-14 20:46:08 +0000

    corrigir89

--- super_trunfo.c
+++ super_trunfo.c
@@ -23,7 +23,7 @@
 
     printf("Super Trunfo em C\n");                                                             // Mostra na tela o nome do jogo
     printf("Carta 1:\n");                                                                     //  Mostra na tela  a carta 1 
-    printf("Estado:  \n");                                                                    // Pede ao usúario a letra do estado
+    printf("Estado:    \n");                                                                    // Pede ao usúario a letra do estado
     scanf("%c",&letra1);                                                                    // Armazena o valor de Estado na variável letra1
 
     printf("Código da carta:  \n");                                                        // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
@@ -69,10 +69,10 @@
     printf("Super Trunfo em C\n");                                                             // Mostra na tela o nome do jogo
     printf("Carta 2:\n");                                                                     //  Mostra na tela  a carta 2 
     printf("Estado:  ");                                                                        // Pede ao usúario a letra do estado
-    scanf("\n%c",&letra2);                                                                  // Armazena o valor de Estado na variável letra2
+    scanf("%c",&letra2);                                                                  // Armazena o valor de Estado na variável letra2
   
     printf("Código da carta: ");                                                         // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
-    scanf("\n%s",codigo2);                                                                                     // Armazena o valor do código da carta na variável código2
+    scanf("%s",codigo2);                                                                 // Armazena o valor do código da carta na variável código2
     
     printf("Nome da cidade:  \n");                                                          // Pede ao usuário o nome da cidade
     scanf("%s",cidade2);                                                                               // Armazena o valor de cidade na variável cidade2
