2a9a436aeef3fc26e411d44e1b621df0a11f8771
LDJESD3-18 <leac162007@gmail.com>
2026-01-14 19:49:23 +0000

    correca013

--- super_trunfo.c
+++ super_trunfo.c
@@ -24,8 +24,7 @@
     printf("Super Trunfo em C\n");                                                             // Mostra na tela o nome do jogo
     printf("Carta 1:\n");                                                                     //  Mostra na tela  a carta 1 
     printf("Estado: ");                                                                       // Pede ao usúario a letra do estado
-    scanf("%c",&letra1); 
-                                                                  // Armazena o valor de Estado na variável letra1
+    scanf("%c",&letra1);                                                              // Armazena o valor de Estado na variável letra1
 
     printf("Código da carta:");                                                         // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
     scanf("%s",codigo1);                                                                // Armazena o valor do código da carta na variável código1
@@ -39,8 +38,7 @@
     printf("\n");                                                        // Armazena o valor de população na variável população1
 
     printf("Área:");                                                                 //Pede ao usuário o número da área da cidade 
-    scanf("%f",&area1); 
-                                                                                   // Armazena o valor da área da cidade na variável área1
+    scanf("%f",&area1);                                                                             // Armazena o valor da área da cidade na variável área1
 
     printf("PIB: ");                                                                  //Pede ao usuário o número do Produto Interno Produto(PIB) 
     scanf("%f",&PIB1);                                                           //Armazena o valor de PIB  em PIB1
@@ -78,12 +76,10 @@
     printf("\n");
 
     printf("Código da carta:");                                                            // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
-    scanf("%s",codigo2); 
-                                                                                        // Armazena o valor do código da carta na variável código2
+    scanf("%s",codigo2);                                                                                     // Armazena o valor do código da carta na variável código2
     
     printf("Nome da cidade: ");                                                           // Pede ao usuário o nome da cidade
-    scanf("%s",cidade2); 
-                                                                                        // Armazena o valor de cidade na variável cidade2
+    scanf("%s",cidade2);                                                                               // Armazena o valor de cidade na variável cidade2
     
 
     printf("População: ");                                                               //Pede ao usuário o número de habitantes da cidade
