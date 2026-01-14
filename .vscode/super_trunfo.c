f9b95e0c873f35fa84db519691ed5170940dea72
LDJESD3-18 <leac162007@gmail.com>
2026-01-14 19:58:43 +0000

    correcao18

--- .vscode/super_trunfo.c
+++ .vscode/super_trunfo.c
@@ -12,25 +12,25 @@
     
     printf("Super Trunfo em C \n");                                                             // Mostra na tela o nome do jogo
     printf("Carta 1:\n");                                                                      //  Mostra na tela  a carta 1 
-    prinf("Estado:\n");                                                                       // Pede ao usúario a letra do estado
+    prinf("Estado:  \n");                                                                       // Pede ao usúario a letra do estado
     scanf("%c",letra1);                                                                      // Armazena o valor de Estado na variável letra1
 
 
-    prinf("Código da carta:\n");                                                            // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
+    prinf("Código da carta:  \n");                                                            // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
     scanf("%c",código1);                                                                   // Armazena o valor do código da carta na variável código1
     
-    prinf("Nome da cidade:\n");                                                              // Pede ao usuário o nome da cidade
+    prinf("Nome da cidade:   \n");                                                              // Pede ao usuário o nome da cidade
     scanf("%s",cidade1);                                                                    // Armazena o valor de cidade na variável cidade1
     
 
-    printf("População:\n");                                                                //Pede ao usuário o número de habitantes da cidade
+    printf("População:   \n");                                                                //Pede ao usuário o número de habitantes da cidade
     scanf("%d",população1);                                                               // Armazena o valor de população na variável população1
 
 
-    printf("Área:\n");                                                                   //Pede ao usuário o número da área da cidade 
+    printf("Área:   \n");                                                                   //Pede ao usuário o número da área da cidade 
     scanf("%f",área1);                                                                  // Armazena o valor da área da cidade na variável área1
 
-    printf("PIB:\n");                                                                  //Pede ao usuário o número do Produto Interno Produto(PIB) 
+    printf("PIB:   \n");                                                                  //Pede ao usuário o número do Produto Interno Produto(PIB) 
     scanf("%f",PIB1);                                                                 //Armazena o valor de PIB  em PIB1
 
 
