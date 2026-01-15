615933c5112fd2fae6e917850963a35f4be34af5
LDJESD3-18 <leac162007@gmail.com>
2026-01-15 19:24:52 +0000

    Alteracao05

--- super_trunfo.c
+++ super_trunfo.c
@@ -26,7 +26,7 @@
     printf("Estado:\n");                                                                    // Pede ao usúario a letra do estado
     scanf(" %c",&letra1);                                                                    // Armazena o valor de Estado na variável letra1
 
-    printf("Código da carta:     \n");                                                        // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
+    printf("Código da Carta:\n");                                                        // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
     scanf(" %s",codigo1);                                                                // Armazena o valor do código da carta na variável código1
   
     printf("Nome da cidade:\n");                                                          // Pede ao usuário o nome da cidade
