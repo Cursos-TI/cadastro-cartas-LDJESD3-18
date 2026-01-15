74e008b64472e92c0d41beffc3416986e2376656
LDJESD3-18 <leac162007@gmail.com>
2026-01-15 19:45:16 +0000

    Alteracao12

--- super_trunfo.c
+++ super_trunfo.c
@@ -24,14 +24,15 @@
     printf("Super Trunfo em C\n");                                                                // Mostra na tela o nome do jogo
     printf("Carta 1:\n");                                                                        //  Mostra na tela  a carta 1 
     printf("Estado:");
-    printf("\n") ;                                                                         // Pede ao usúario a letra do estado
+    printf("\n") ;                                                                              // Pede ao usúario a letra do estado
     scanf(" %c",&letra1);                                                                      // Armazena o valor de Estado na variável letra1
      
-    printf("Código da carta:\n");
-    printf("\n");                                                                           // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
+    printf("Código da carta:");                                                              // Pede ao usuário o código da carta(sequência formada pela letra do estado e sequência de 01 a 04(ex;A01)  )
+    printf("\n");                                                                          
     scanf(" %s",codigo1);                                                                  // Armazena o valor do código da carta na variável código1
   
-    printf("Nome da cidade:\n");                                                          // Pede ao usuário o nome da cidade
+    printf("Nome da cidade:");                                                            // Pede ao usuário o nome da cidade
+    printf("\n") ;                                                        
     scanf(" %s",cidade1);                                                                // Armazena o valor de cidade na variável cidade1
   
     printf("População:\n");                                                             //Pede ao usuário o número de habitantes da cidade
